42d57b9d7b18d280a3ed6d3916bdf49ca5788685
ARIF RUBAYET <72675261+arub9090@users.noreply.github.com>
2022-04-06 14:48:56 -0400

    Create momoizationDynamicProgramming.js
    Fibo number with Dynamic Programming using The Memoization Method

---
+++ momoizationDynamicProgramming.js
@@ -0,0 +1,7 @@
+function fibo(n, memo=[]) {
+    if(memo[n] !== undefined) return memo[n]
+    if(n <=2) return 1;
+    var res= fibo(n-1, memo)+fibo(n-2, memo)
+    memo[n]= res;
+    return res;
+}
